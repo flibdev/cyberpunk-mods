4854b943f70d1dcf340430c6034be3bf8d142e76
Reece Sheppard <reecesheppard@gmail.com>
2021-05-11 01:36:10 +0800

    Updated build scripts

---
+++ UI-Improvements/build.ps1
@@ -0,0 +1,27 @@
+function Build-ModArtifact {
+    param (
+        $TargetPath,
+        $ListFile
+    )
+    
+    $7z = Get-Command "7z.exe"
+
+    Remove-Item $TargetPath -ErrorAction Ignore
+
+    & $7z.Source a $TargetPath -tzip @$ListFile -bso0 -bsp0 -bse1
+
+    if ($LastExitCode -gt 0) {
+        Remove-Item $TargetPath -ErrorAction Ignore
+        
+        throw "7Zip didn't run flawlessly"
+    }
+    else {
+        Write-Output "Built artifact: $TargetPath"
+    }
+}
+
+Write-Output "Building FOMOD"
+Build-ModArtifact -TargetPath "build\UI-Improvements.fomod" -ListFile "build-fomod.txt"
+
+Write-Output "`nBuilding Recommended"
+Build-ModArtifact -TargetPath "build\UI-Improvements-Recommended.zip" -ListFile "build-recommended.txt"
